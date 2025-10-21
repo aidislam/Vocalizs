7974db1b3b42a4420a803bc4ef44f63874a5d61d
Aid Islam <aidislam27@gmail.com>
2025-10-21 12:31:18 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,14 +1,8 @@
-'use client';
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAWDt1G1F0B8KoJn95CAD8LchKIDtLhKsw',
-  authDomain: 'aid-islam-account.firebaseapp.com',
-  databaseURL: 'https://aid-islam-account-default-rtdb.firebaseio.com',
-  projectId: 'aid-islam-account',
-  storageBucket: 'aid-islam-account.firebasestorage.app',
-  messagingSenderId: '82633858382',
-  appId: '1:82633858382:web:2c8800d45d7a5e020dd21f',
-  measurementId: 'G-9B4PSKYG7P',
+  "projectId": "studio-814518738-6a892",
+  "appId": "1:1006582407847:web:99e73bec43fec2c6e77182",
+  "apiKey": "AIzaSyAOwUyrC_ItsjmiiVaRkYPCNzKEu5V27Hw",
+  "authDomain": "studio-814518738-6a892.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1006582407847"
 };
